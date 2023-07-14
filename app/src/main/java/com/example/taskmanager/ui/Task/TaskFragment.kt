d593e5d266308d47f027881ab9f890f3ade39517
TakaYouKii <avwro1708@gmaol.cpm>
2023-07-14 16:07:34 +0600

    lesson4.1

---
+++ app/src/main/java/com/example/taskmanager/ui/Task/TaskFragment.kt
@@ -0,0 +1,48 @@
+package com.example.taskmanager.ui.Task
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import androidx.navigation.fragment.findNavController
+import com.example.taskmanager.R
+import com.example.taskmanager.databinding.ActivityMainBinding
+import com.example.taskmanager.databinding.FragmentTaskkBinding
+import com.example.taskmanager.model.Task
+
+
+class TaskFragment : Fragment() {
+
+    private lateinit var binding: FragmentTaskkBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentTaskkBinding.inflate(inflater,container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSave.setOnClickListener{
+            val data = Task(
+                title = binding.etTitle.text.toString(),
+                desk = binding.etDescription.text.toString(),
+            )
+
+            setFragmentResult(RESULT_REQUEST_KEY, bundleOf(RESULT_KEY to data))
+            findNavController().navigateUp()
+        }
+    }
+
+    companion object{
+        const val RESULT_REQUEST_KEY = "request.key"
+        const val RESULT_KEY = "result.key"
+    }
+
+
+}
